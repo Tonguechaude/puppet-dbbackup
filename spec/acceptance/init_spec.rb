76e96dae945eed604cffe63711741249f0e4aa1e
evan challias <evan.challias@adullact.org>
2025-05-28 17:57:12 +0200

    wip

--- spec/acceptance/init_spec.rb
+++ spec/acceptance/init_spec.rb
@@ -3,13 +3,19 @@
 require 'spec_helper_acceptance'
 
 describe 'dbbackup' do
-  context 'with a backup server' do
+  let(:date) { Time.now.strftime('%Y-%m-%d') }
+
+  context 'with basic params' do
+    let(:backup_dir) { "/mnt/dumps/#{date}" }
     let(:pp) do
       'include dbbackup'
     end
 
-    it 'works idempotently with no errors' do
+    it 'applies without errors' do
       apply_manifest(pp, catch_failures: true)
+    end
+
+    it 'applies idempotently' do
       apply_manifest(pp, catch_changes: true)
     end
 
@@ -20,6 +26,80 @@ describe 'dbbackup' do
 
     describe file('/usr/local/bin/dump_databases') do
       it { is_expected.to be_executable }
+    end
+
+    it 'forces the backup service to run' do
+      result = shell('systemctl start dump_databases.service')
+      expect(result.exit_code).to eq(0)
+    end
+
+    describe command('/usr/local/bin/dump_databases') do
+      its(:exit_status) { is_expected.to eq 0 }
+    end
+
+    it 'runs the dump script and creates the backup dumps' do
+      tries = 0
+      until File.directory?(backup_dir) || tries >= 10
+        sleep(6)
+        tries += 1
+      end
+
+      raise "Backup directory #{backup_dir} does not exist after waiting" unless File.directory?(backup_dir)
+
+      dumps1 = Dir.glob("#{backup_dir}/*.mysql.gz")
+      dumps2 = Dir.glob("#{backup_dir}/*.psql.gz")
+      expect(dumps1).not_to be_empty
+      expect(dumps2).not_to be_empty
+    end
+  end
+
+  context 'with custom params' do
+    let(:backup_custom_dir) { "/var/dumps/#{date}" }
+    let(:pp) do
+      "class { 'dbbackup':
+        destination => '/var/dumps'
+      }"
+    end
+
+    it 'applies without errors' do
+      apply_manifest(pp, catch_failures: true)
+    end
+
+    it 'applies idempotently' do
+      apply_manifest(pp, catch_changes: true)
+    end
+
+    describe service('dump_databases.timer') do
+      it { is_expected.to be_enabled }
+      it { is_expected.to be_running }
+    end
+
+    describe file('/usr/local/bin/dump_databases') do
+      it { is_expected.to be_executable }
+    end
+
+    it 'forces the backup service to run' do
+      result = shell('systemctl start dump_databases.service')
+      expect(result.exit_code).to eq(0)
+    end
+
+    describe command('/usr/local/bin/dump_databases') do
+      its(:exit_status) { is_expected.to eq 0 }
+    end
+
+    it 'runs the dump script and creates the backup dumps' do
+      tries = 0
+      until File.directory?(backup_custom_dir) || tries >= 10
+        sleep(6)
+        tries += 1
+      end
+
+      raise "Backup directory #{backup_custom_dir} does not exist after waiting" unless File.directory?(backup_custom_dir)
+
+      dumps1 = Dir.glob("#{backup_custom_dir}/*.mysql.gz")
+      dumps2 = Dir.glob("#{backup_custom_dir}/*.psql.gz")
+      expect(dumps1).not_to be_empty
+      expect(dumps2).not_to be_empty
     end
   end
 end
